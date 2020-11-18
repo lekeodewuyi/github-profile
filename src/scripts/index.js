19c3969a7259909c8d4d58eb0cd12d814f9723b9
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-11-18 17:40:10 -0500

    feat(func): initializes and sets user status based on retrieved data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -96,13 +96,60 @@ const retrieveData = (username) => {
   
   }
   
-  retrieveData(githubData.username);
+retrieveData(githubData.username);
 
-  newUserName.addEventListener('keyup', function(){
+newUserName.addEventListener('keyup', function(){
     retrieveData(newUserName.value);
-  })
+})
+
+newUserName.addEventListener('blur', function(){
+    retrieveData(newUserName.value);
+})
+
+
+let status = {
+    emojiHTML: null,
+    message: null
+  }
   
-  newUserName.addEventListener('blur', function(){
-    retrieveData(newUserName.value);
-  })
+  
+const updateStatus = (data) => {
+    status = {
+        emojiHTML: data.emojiHTML,
+        message: data.message
+    }
+}
+  
+const setStatusIcon = document.querySelectorAll('.set-status-icon');
+const setStatusText = document.querySelectorAll('.set-status-text');
+  
+const appendStatus = (data) => {
+    data = status;
+    console.log("Im here y'all")
 
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    console.log(vw)
+
+    if (data.emojiHTML !==null) {
+        setStatusIcon.forEach((icon) => {
+            icon.innerHTML = data.emojiHTML;
+        })
+    } else {
+        setStatusIcon.forEach((icon) => {
+            icon.innerHTML = `<svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"></path></svg>`;
+        })
+    }
+
+    if (data.message !==null) {
+        setStatusText.forEach((text) => {
+            text.innerText = truncateText(data.message, 42);
+        if (vw > 768) {
+            text.innerText = truncateText(data.message, 10);
+        }
+    })
+    } else {
+        setStatusText.forEach((text) => {
+            text.innerText = "Set status";
+        })
+    }
+}
