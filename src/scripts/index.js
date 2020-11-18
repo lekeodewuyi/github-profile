ba9bfbe1af5e0672d0e50043da4b2d7eac872a28
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-11-18 17:29:28 -0500

    feat: adds function to show/hide prompt on page load

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,25 @@
 import {queryFunc, githubData} from './util/github_data.js';
 import {isInViewport, relativeTime, appendChildFunc, appendStar, truncateText} from './util/helper.js';
+
+
+const checkbox = document.querySelector('.checkbox');
+const loader = document.querySelector('.loader');
+
+const modal = document.querySelector('.modal');
+const openModal = document.querySelectorAll('.open-modal');
+const closeModal = document.querySelector('.modal-close-btn');
+const newUserName = document.querySelector('.modal-input');
+const nowShowing = document.querySelector('.modal-now-showing');
+
+
+const checkPrompStatus = () => {
+    const dontShow = localStorage.getItem('dontShow');
+    if (dontShow === "true") {
+        modal.classList.add('hide');
+        checkbox.checked = true;
+    } else {
+        modal.classList.remove('hide');
+    }
+}
+
+checkPrompStatus()
